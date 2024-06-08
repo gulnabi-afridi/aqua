a23a0e4212160800b8d0567d11f5f1207620f0a6
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-06-09 00:40:52 +0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+// import Userform from "../components/Form/Index";
+
+const Home: NextPage = () => {
+  return <>{/* <Userform /> */}</>;
+};
+
+export default Home;
